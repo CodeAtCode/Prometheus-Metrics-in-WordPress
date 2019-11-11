dc26158eb5aa8c06135d2a71ecac173fc1ff60ad
Daniele Scasciafratte <mte90net@gmail.com>
2019-11-11 17:32:10 +0100

    first upload

---
+++ prometheus-metrics-for-wp.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Plugin Name: Prometheus Metrics in WordPress
+ * Plugin URI: https://github.com/codeatcode/prometheus-metrics-for-wp/
+ * Description: Add a custom json endpoint for Prometheus
+ * Version: 1.0
+ */
+     
+add_filter( 'rest_pre_serve_request', 'prometheus_serve_request', 10, 4 );
+add_action( 'rest_api_init', 'prometheus_register_route' );
+
+function prometheus_get_metrics() {
+	global $wpdb, $table_prefix;
+
+	if ( isset( $_GET[ 'users' ] ) && esc_html( $_GET[ 'users' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$users   = count_users();
+		$result  = "# HELP wp_users_total Total number of users.\n";
+		$result .= "# TYPE wp_users_total counter\n";
+		$result .= 'wp_users_total ' . $users[ 'total_users' ] . "\n";
+	}
+
+	$posts       = wp_count_posts();
+	$n_posts_pub = $posts->publish;
+	$n_posts_dra = $posts->draft;
+
+	if ( isset( $_GET[ 'posts' ] ) && esc_html( $_GET[ 'posts' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$result .= 'wp_posts_total{status="published"} ' . $n_posts_pub . "\n";
+		$result .= "# HELP wp_posts_draft_total Total number of posts published.\n";
+		$result .= "# TYPE wp_posts_draft_total counter\n";
+		$result .= 'wp_posts_total{status="draft"} ' . $n_posts_dra . "\n";
+	}
+
+	if ( isset( $_GET[ 'pages' ] ) && esc_html( $_GET[ 'pages' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$n_pages = wp_count_posts( 'page' );
+		$result .= "# HELP wp_pages_total Total number of posts published.\n";
+		$result .= "# TYPE wp_pages_total counter\n";
+		$result .= 'wp_pages_total{status="published"} ' . $n_pages->publish . "\n";
+		$result .= 'wp_pages_total{status="draft"} ' . $n_pages->draft . "\n";
+	}
+
+	if ( isset( $_GET[ 'autoload' ] ) && esc_html( $_GET[ 'autoload' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$query   = $wpdb->get_results( 'SELECT * FROM `' . $table_prefix . "options` WHERE `autoload` = 'yes'", ARRAY_A ); // phpcs:ignore WordPress.DB
+		$result .= "# HELP wp_options_autoload Options in autoload.\n";
+		$result .= "# TYPE wp_options_autoload counter\n";
+		$result .= 'wp_options_autoload ' . count( $query ) . "\n";
+	}
+
+	if ( isset( $_GET[ 'transient' ] ) && esc_html( $_GET[ 'transient' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$query   = $wpdb->get_results( 'SELECT * FROM `' . $table_prefix . "options` WHERE `autoload` = 'yes' AND `option_name` LIKE '%transient%'", ARRAY_A ); // phpcs:ignore WordPress.DB
+		$result .= "# HELP wp_transient_autoload DB Transient in autoload.\n";
+		$result .= "# TYPE wp_transient_autoload counter\n";
+		$result .= 'wp_transient_autoload ' . count( $query ) . "\n";
+	}
+
+	if ( isset( $_GET[ 'user_sessions' ] ) && esc_html( $_GET[ 'user_sessions' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$query   = $wpdb->get_results( 'SELECT * FROM `' . $table_prefix . "options` WHERE `option_name` LIKE '_wp_session_%'", ARRAY_A ); // phpcs:ignore WordPress.DB
+		$result .= "# HELP wp_user_sessions User sessions.\n";
+		$result .= "# TYPE wp_user_sessions counter\n";
+		$result .= 'wp_user_sessions ' . count( $query ) . "\n";
+	}
+
+	if ( isset( $_GET[ 'posts_without_title' ] ) && esc_html( $_GET[ 'posts_without_title' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$query   = $wpdb->get_results( 'SELECT * FROM `' . $table_prefix . "posts` WHERE post_title='' AND post_status!='auto-draft' AND post_status!=\'draft\' AND post_status!=\'trash\' AND (post_type='post' OR post_type='page')", ARRAY_A ); // phpcs:ignore WordPress.DB
+		$result .= "# HELP wp_posts_without_title Post/Page without title.\n";
+		$result .= "# TYPE wp_posts_without_title counter\n";
+		$result .= 'wp_posts_without_title ' . count( $query ) . "\n";
+	}
+
+	if ( isset( $_GET[ 'posts_without_content' ] ) && esc_html( $_GET[ 'posts_without_content' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$query   = $wpdb->get_results( 'SELECT * FROM `' . $table_prefix . "posts` WHERE post_content='' AND post_status!='draft' AND post_status!='trash' AND post_status!='auto-draft' AND (post_type='post' OR post_type='page')", ARRAY_A ); // phpcs:ignore WordPress.DB
+		$result .= "# HELP wp_posts_without_content Post/Page without content.\n";
+		$result .= "# TYPE wp_posts_without_content counter\n";
+		$result .= 'wp_posts_without_content ' . count( $query ) . "\n";
+	}
+
+	if ( isset( $_GET[ 'db_size' ] ) && esc_html( $_GET[ 'db_size' ] ) === 'yes' ) { // phpcs:ignore WordPress.Security
+		$query   = $wpdb->get_results( "SELECT SUM(ROUND(((data_length + index_length) / 1024 / 1024), 2)) FROM information_schema.TABLES WHERE table_schema = '" . DB_NAME . "'", ARRAY_A ); // phpcs:ignore WordPress.DB
+		$result .= "# HELP wp_db_size Total DB size in MB.\n";
+		$result .= "# TYPE wp_db_size counter\n";
+		$result .= 'wp_db_size ' . $query[ 0 ][ 'SUM(ROUND(((data_length + index_length) / 1024 / 1024), 2))' ] . "\n";
+	}
+
+	return $result;
+	}
+
+function prometheus_empty_func() {
+	return '{ "error": "You cannot access to that page" }';
+	}
+
+function prometheus_serve_request( $served, $result, $request, $server ) {
+	if ( !defined( 'PROMETHEUS_KEY' ) && $request->get_route() === '/metrics' ) {
+		echo prometheus_empty_func(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		$served = true;
+	}
+
+	if ( isset( $_GET[ 'prometheus' ] ) && esc_html( $_GET[ 'prometheus' ] ) === PROMETHEUS_KEY ) { // phpcs:ignore WordPress.Security
+		header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+		$metrics = prometheus_get_metrics();
+		echo $metrics; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		$served = true;
+	}
+
+	return $served;
+	}
+
+function prometheus_register_route() {
+	register_rest_route(
+		'metrics',
+		'/',
+		array(
+            'methods'  => 'GET',
+            'callback' => 'prometheus_empty_func',
+		)
+	);
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Prometheus Metrics in WordPress
+
+## Settings  
+
+In wp-config.php you need to settings that constant that will be used to expose those metrics in the url.
+
+`define( 'PROMETHEUS_KEY', 'i8w374sdkfjg' );`
+
+The url: http://domain.tld/wp-json/metrics?prometheus=i8w374sdkfjg
+
+### URL parameters
+
+
+## Output
+
